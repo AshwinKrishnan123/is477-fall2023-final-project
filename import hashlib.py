206fa279e9cd9fc7ea464ebdeabdda7c10d7e343
AshwinKrishnan123 <112290906+AshwinKrishnan123@users.noreply.github.com>
2023-12-02 15:33:11 -0600

    Update import hashlib.py

--- import hashlib.py
+++ import hashlib.py
@@ -1,17 +1,17 @@
 import hashlib
 import requests
 
-url ='https://archive.ics.uci.edu/static/public/53/iris.zip'
+url ='https://archive.ics.uci.edu/static/public/862/turkish+music+emotion.zip'
 response = requests.get(url)
 
-with open ('iris.zip', mode='wb') as f:
+with open ('turkish+music+emotion.zip', mode='wb') as f:
     f.write(response.content)
 
-filename= 'iris.zip'
+filename= 'turkish+music+emotion.zip.zip'
 with open (filename, mode='rb') as f:
     data=f.read()
     sha256hash = hashlib.sha256(data).hexdigest()
-    uci_iris = '4ba9ff91a7586a37383329548823a4584298716a51f786c55a273c1c201571c2'
+    uci_iris = 'cde69ba13c7968200e5dc904c863b0a41c3e309977a8c60966d4ac57cd9247cc'
 if (uci_iris !=sha256hash):
     print('Computed hash does not match expected hash')
 else:
