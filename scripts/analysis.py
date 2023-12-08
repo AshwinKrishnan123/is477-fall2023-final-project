7bfb4c3df420c0b342a5deef72e5d6f18f88c3d9
AshwinKrishnan123 <112290906+AshwinKrishnan123@users.noreply.github.com>
2023-12-08 17:16:18 -0600

    Analysis

---
+++ scripts/analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+plt.figure(figsize=(10, 6))
+sns.regplot(x='Class', y='_HarmonicChangeDetectionFunction_PeriodEntropy', data=df, scatter_kws={'s': 20, 'alpha': 0.5})
+plt.title('Regression Plot of Harmonic Change Detection Function Period Entropy')
+plt.xlabel('Class')
+plt.ylabel('Period Entropy')
+plt.show()
