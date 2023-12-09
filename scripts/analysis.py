13a4ba61b949348807d534a389acaeb6160c33a0
a0ybig <stefanchu@live.com>
2023-12-08 21:47:09 -0600

    PLOT

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../data/Acoustic Features.csv")
+df = pd.read_csv("data/Acoustic Features.csv")
 df['Class'] = pd.Categorical(df['Class']).codes
 df['_HarmonicChangeDetectionFunction_PeriodEntropy'] = pd.to_numeric(df['_HarmonicChangeDetectionFunction_PeriodEntropy'], errors='coerce')
 df = df.dropna(subset=['_HarmonicChangeDetectionFunction_PeriodEntropy'])
@@ -12,4 +12,4 @@ sns.regplot(x='Class', y='_HarmonicChangeDetectionFunction_PeriodEntropy', data=
 plt.title('Regression Plot of Harmonic Change Detection Function Period Entropy')
 plt.xlabel('Class')
 plt.ylabel('Period Entropy')
-plt.show()
+plt.savefig('results/plot.png')
