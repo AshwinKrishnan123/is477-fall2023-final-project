149baf9eb38489f46a47232bd37793946de3dc65
a0ybig <stefanchu@live.com>
2023-12-08 19:37:12 -0600

    license

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/Acoustic Features 2.csv")
+df = pd.read_csv("../data/Acoustic Features 2.csv")
 
 plt.figure(figsize=(10, 6))
 sns.regplot(x='Class', y='_HarmonicChangeDetectionFunction_PeriodEntropy', data=df, scatter_kws={'s': 20, 'alpha': 0.5})
